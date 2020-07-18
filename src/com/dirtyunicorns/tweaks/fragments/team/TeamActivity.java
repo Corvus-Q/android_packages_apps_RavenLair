7d5f495ff2dcccc4866763faa186ae9e3d516269
azaharuddin <mdazahar.2008@gmail.com>
2020-07-19 02:09:40 +0530

    Lair: team: Add @Azaharuddin in maintainer...
    Change-Id: I3cbbaf41f8a7323f8796b445834df81023ce4ed5

--- src/com/dirtyunicorns/tweaks/fragments/team/TeamActivity.java
+++ src/com/dirtyunicorns/tweaks/fragments/team/TeamActivity.java
@@ -94,6 +94,8 @@ public class TeamActivity extends Activity {
                 R.drawable.jeangraff);
         setTeamMember("Mahmoud", getString(R.string.maintainer_title), "MahmoudK1000", "MahmoudK1000",
                 R.drawable.mahmoud);
+        setTeamMember("Azaharuddin", getString(R.string.maintainer_title), "aza786", "Azahar123",
+                R.drawable.azahar);
         setTeamMember("Shekhar", getString(R.string.maintainer_title), "Flamefusion", "Flamefusion",
                 R.drawable.shekhar);
         setTeamMember("Sujit", getString(R.string.maintainer_title), "kumarsujita6", "kumarsujita6",
