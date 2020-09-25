3a81683843721c3ef73f093dd717706efc7c6e90
maxwen <max.weninger@gmail.com>
2020-09-25 23:50:04 +0530

    Lair: Global Actions with Advanced Reboot [2/2]
    Co-authored-by: merser2005 <merser2005@gmail.com>
    Change-Id: I2f57ead2589c37630f90228d4aee9e054047034a

--- src/com/raven/lair/fragments/PowerMenu.java
+++ src/com/raven/lair/fragments/PowerMenu.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (C) 2017-2019 The Dirty Unicorns Project
+ * Copyright (C) 2014-2015 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ * Copyright (C) 2019 RevengeOS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,63 +18,56 @@
 
 package com.raven.lair.fragments;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.UserInfo;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.provider.SearchIndexableResource;
+import android.os.UserManager;
 import android.provider.Settings;
-import androidx.preference.*;
+
+import androidx.preference.PreferenceCategory;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
 
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.internal.widget.LockPatternUtils;
-
 import com.android.settings.R;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class PowerMenu extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener, Indexable {
+public class PowerMenu extends SettingsPreferenceFragment implements
+         Preference.OnPreferenceChangeListener {
+
+    final static String TAG = "PowerMenuActions";
+
+    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.powermenu);
-
-        final PreferenceScreen prefSet = getPreferenceScreen();
+        PreferenceScreen prefSet = getPreferenceScreen();
 
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        return false;
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return true;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CORVUS;
     }
-
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                        boolean enabled) {
-                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
-                    final SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.powermenu;
-                    result.add(sir);
-                    return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    final List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
-        }
-    };
 }
+
